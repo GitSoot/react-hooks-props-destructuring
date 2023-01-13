1231e5cf6067102590e93d9d0d74b5d9485bbce6
Eric Onyango <eric.onyango@student.moringaschool.com>
2023-01-13 05:07:54 +0300

    Completed assignment

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -4,12 +4,15 @@ function MovieCard({
   title,
   posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
   genres,
+  runTime,
 }) {
   return (
     <div className="movie-card">
       <img src={posterSrc} alt={title} />
       <h2>{title}</h2>
       <small>{genres.join(", ")}</small>
+      <br></br>
+      <small>Runtime: {runTime.join(", ")}</small>
     </div>
   );
 }
